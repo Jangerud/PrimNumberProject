5b3c12833bfbeb3bbc805002d720e601773dabb5
jange <jange@DESKTOP-1PGUA82>
2021-10-06 12:13:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimNumberProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
